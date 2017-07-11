4df3118b30818111d5b98438c1e37b7706c7e912
Carlos Anguiano <c.joseanguiano@gmail.com>
2017-07-11 08:02:03 -0500

    Following example demonstrates how to add first n natural numbers by using the concept of stack.

---
+++ src/com/devix/www/Main.java
@@ -0,0 +1,61 @@
+package com.devix.www;
+
+import java.io.IOException;
+import java.util.Stack;
+
+public class Main {
+
+    static int num;
+    static int ans;
+    static Stack theStack;
+
+    public static void main(String[] args)
+
+            throws IOException {
+        num = 50;
+        stackAddition();
+        System.out.println("Sum = " + ans);
+
+    }
+
+    private static void stackAddition() {
+        theStack = new Stack(10000);
+        ans = 0;
+        while (num > 0) {
+            theStack.push(num);
+            --num;
+        }
+        while (!theStack.isEmpty()) {
+            int newN = (int) theStack.pop();
+            ans += newN;
+        }
+    }
+
+    static class Stack {
+        private int maxSize;
+        private int[] data;
+        private int top;
+
+        public Stack(int s) {
+            maxSize = s;
+            data = new int[maxSize];
+            top = -1;
+        }
+
+        public void push(int p) {
+            data[++top] = p;
+        }
+
+        public int pop() {
+            return data[top--];
+        }
+
+        public int peek() {
+            return data[top];
+        }
+
+        public boolean isEmpty() {
+            return (top == -1);
+        }
+    }
+}
